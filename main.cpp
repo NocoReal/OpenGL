459c83a08c3cde4fc5a71b8173454802c00b82b9
NocoReal <alexandru2008vladu@gmail.com>
2025-03-20 14:21:28 +0200

    simple window

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+int WIDTH=1280, HEIGHT=720;
+using namespace std;
+int main() {
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "OpenGL", NULL, NULL);
+	if (window == NULL) {
+		cout << "Failed to create GLFW window" << endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	glewInit();
+
+	if (glewInit() != GLEW_OK) {
+		cout << "Failed to initialize glew" << endl;
+		glfwTerminate();
+		return -2;
+	}
+
+	glViewport(0, 0, WIDTH, HEIGHT);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+
+	}
+
+	glfwTerminate();
+	return 0;
+}
